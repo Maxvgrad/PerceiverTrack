ee44d77472ed1a885706baa92d1b50cd6bc14cfb
Maksim Ploter <maksim.ploter@gmail.com>
2024-09-09 12:31:18 +0300

    fix: add num of classes for nuimages

--- src/trackformer/models/__init__.py
+++ src/trackformer/models/__init__.py
@@ -40,6 +40,8 @@ def build_model(args):
             # We are only interested in 1 clas Person,
             # so N is 1, but according to DETR paper we add 1 (account for zero indexed array)
             num_classes = 2
+    elif args.dataset == 'nuimages':
+        num_classes = 10 # 9 + 1 (account for zero indexed array)
     else:
         raise NotImplementedError
 
