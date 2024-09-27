7e700cb320fc938ea7ee6574d8ec15902dc20c35
Maksim Ploter <maksim.ploter@gmail.com>
2024-09-27 14:34:42 +0300

    fix: remove extra dimension

--- src/trackformer/models/deformable_transformer.py
+++ src/trackformer/models/deformable_transformer.py
@@ -380,8 +380,7 @@ class DeformableTransformerDecoderLayer(nn.Module):
             # mask_all_ones will be a [batch_size] tensor of True/False
             mask_all_ones = src_padding_mask.view(src_padding_mask.size(0), -1).all(dim=1)
 
-            # Expand the mask to the shape [batch_size, 1, 1, 1] to cover the channels, H, and W dimensions
-            mask_all_ones = mask_all_ones.view(-1, 1, 1, 1)
+            mask_all_ones = mask_all_ones.view(-1, 1, 1)
 
             # Zero out the corresponding rows in tgt2
             tgt2 = tgt2.masked_fill(mask_all_ones, float(0))
